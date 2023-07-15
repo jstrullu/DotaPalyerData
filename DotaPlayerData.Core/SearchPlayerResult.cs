df5ac54c3fb1153c4c4efb95e947c2582a4a62ba
Julien Strullu <julien.strullu@gmail.com>
2023-07-15 16:45:58 +0200

    ajout recherche par nom

---
+++ DotaPlayerData.Core/SearchPlayerResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DotaPlayerData.Core;
+
+public class SearchPlayerResult
+{
+    [JsonPropertyName("account_id")]
+    public int AccountId { get; set; }
+
+    [JsonPropertyName("avatarfull")]
+    public string AvatarFull { get; set; }
+    
+    [JsonPropertyName("personaname")] 
+    public string PersonaName { get; set; }
+    
+    [JsonPropertyName("last_match_time")]
+    public string LastMatchTime { get; set; }
+    
+    [JsonPropertyName("similarity")]
+    public int Similarity { get; set; }
+}
